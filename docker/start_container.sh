c7b5cf250b0b6b6d74748449cd096e50edb6a302
Marvin Meiers <m.mrs@protonmail.com>
2023-08-31 13:42:09 +0200

    docker: support relative paths for start_container.sh

--- docker/start_container.sh
+++ docker/start_container.sh
@@ -3,7 +3,7 @@
 # This script expects as first argument the path to a gitinspector source folder
 # and as second argument the path to a git repository
 
-gitinspector_src=$1
-repo=$2
+gitinspector_src=$(readlink -f $1)
+repo=$(readlink -f $2)
 
 docker run --user inspector --rm -it -v $repo:/repo -v $gitinspector_src:/gitinspector -w /repo gitinspector /bin/bash
